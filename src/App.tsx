2ae4099d57b6153f6613801353e70fbe136bf20f
Matheus Henrique <matheus.bezerra@senado.leg.br>
2022-11-08 13:49:52 -0300

    Extensão de tipagem do styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </ThemeProvider>
   );
 }
